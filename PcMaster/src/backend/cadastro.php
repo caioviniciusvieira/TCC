9d281c3b8b16df2887a087aa2ff964bf3e02db1a
caioviniciusvieira <caioviniciusrsvieira15@gmail.com>
2025-09-23 19:16:44 +0000

    removi p html

--- PcMaster/src/backend/cadastro.php
+++ PcMaster/src/backend/cadastro.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+ <?php
     session_start();
     include_once('conexao.php');
 
@@ -37,5 +29,3 @@
     $conexao->close();
     
     ?>
-</body>
-</html>
--- PcMaster/src/backend/login.php
+++ PcMaster/src/backend/login.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
     session_start();
 
     include_once('conexao.php');
@@ -36,6 +28,4 @@
     $stmt->close();
     $conexao->close();
 
-    ?>
-</body>
-</html>
+?>
